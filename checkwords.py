165b1f5506a59fbdb00d80a5f6e650377e0ba4c0
Otse Obande <otseobande@gmail.com>
2018-09-16 17:33:55 +0100

    optimize speed

--- checkwords.py
+++ checkwords.py
@@ -1,7 +1,11 @@
 from nltk.corpus import words
 from itertools import permutations, combinations
+from typeguard import typechecked
 
-def get_possible_words(letters):
+word_set = set(words.words())
+
+@typechecked
+def get_possible_words(letters: str) -> list:
   '''
   This function arranges the letters in all possible ways,
   checks each arrangement if it's an English word
@@ -10,10 +14,14 @@ def get_possible_words(letters):
   possible_words = []
 
   for i in range(2, len(letters) + 1):
-    for c in combinations(letters, i):
-      for p in permutations(''.join(c)):
-        potential_word = ''.join(p)
-        if potential_word in words.words():
-          possible_words.append(potential_word)
+    letter_combinations = set(combinations(letters, i))
+
+    for c in letter_combinations:
+      unique_permutations = set(permutations(''.join(c)))
+
+      for p in unique_permutations:
+        word = ''.join(p)
+        if word in word_set and word not in set(possible_words):
+          possible_words.append(word)
 
   return possible_words
--- tests/test_checkwords.py
+++ tests/test_checkwords.py
@@ -3,10 +3,15 @@ from checkwords import get_possible_words
 
 class TestCheckWordsFunctions(unittest.TestCase):
   def test_get_possible_words(self):
-    possible_words = get_possible_words('flag')
-    words = ['fa', 'la', 'al', 'ga', 'alf', 'fag', 'lag', 'gal', 'flag']
+    words = get_possible_words('flag')
 
-    self.assertEqual(possible_words, words)
+    self.assertEqual(len(words), 9)
+
+  def test_words_are_not_repeated(self):
+    words = get_possible_words('ggood')
+    no_duplicates = len(words) == len(set(words))
+
+    self.assertTrue(no_duplicates)
 
 if __name__ == '__main__':
   unittest.main()
